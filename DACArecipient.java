f1a34169fa8886bf9e26958d2b3ae84886f4e247
Raul Soto <rsoto@swccd.edu>
2026-08-17 15:23:44 -0700

    Updated Lab

---
+++ DACArecipient.java
@@ -0,0 +1,61 @@
+//TODO: Update UML class diagram
+/* UML CLASS DIAGRAM:
+-----------------------------------------
+					DACArecipient
+-----------------------------------------
+- surname : String
+- givenName : String
+- uscisNumber : String
+- countryOfOrigin : String
+- birthday : int
+- validFromDate : int
+- expirationDate : int
+- sex : char
+-----------------------------------------
++ getSurname() : String
++ getGivenName() : String
++ getUscisNumber() : String
++ getCountyOfOrigin() : String
++ getBirthDay() : int
++ getValidFromDate() : int
++ getExpirationDate() : int
++ getSex() : char
++ setSurname(surname : String) : void
++ setGivenName(givenName : String) : void
++ setUscisNumber(uscisNumber : String) : void
++ setCountryOfOrigin(countryOfOrigin : String) : void
++ setBirthday(birthday : int) : void
++ setValidFromDate(validFromDate : int) : void
++ setExpirationDate(expirationDate : int) : void
++ setSex(sex : char) : void
++ setAll(surname:String, givenName:String, uscisNumber:String, countryOfOrigin:String, birthday:int, validFromDate:int, expirationDate:int,sex:char) : void
++ toString() : String
++ equals(other : DACArecipient) : boolean
++ printCard() : void
++ jdnToDate(J : int) : String
+-----------------------------------------
+*/
+
+/**
+ * Represents one person receiving deferred action for childhood arrivals (DACA).
+ *
+ * @author Your name here
+ * @version 1.2
+ */
+public class DACArecipient {
+    /***** INSTANCE VARIABLES *****/
+    private String surname;
+    private String givenName;
+    private String uscisNumber;
+    private String countryOfOrigin;
+    private int birthday, validFromDate, expirationDate;
+    private char sex;
+
+    /**** CONSTRUCTORS ****/
+    //TODO: Part 1- Write the no-argument constructor. Remember to include documentation for each constructor.
+
+    //TODO: Part 1- Write the full constructor. Remember to include documentation for each constructor.
+
+    //TODO: Copy all your methods from the previous labs!
+
+}
---
+++ Main.java
@@ -0,0 +1,12 @@
+/**
+ * Your program description here
+ * 
+ * @author Your name here
+ */
+public class Main 
+{
+	public static void main(String[] args) 
+	{
+		// TODO: Part 3 - Test the DACArecepient constructors
+	}
+}
